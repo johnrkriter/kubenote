8a36ce876bc2fd8709f900b5ec5003c7bcdd9fa0
Wai Loon <wai.loon.theng@accenture.com>
2019-04-19 01:00:52 +0800

    Removed redundant annotation

--- microservice-boilerplate-sample/src/main/java/com/example/MicroserviceBoilerplateApplication.java
+++ microservice-boilerplate-sample/src/main/java/com/example/MicroserviceBoilerplateApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAspectJAutoProxy
 public class MicroserviceBoilerplateApplication {
 
 	public static void main(String[] args) {
